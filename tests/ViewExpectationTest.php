1bf60d838645e8c0e6ca939df10bcc2c789740ce
Bastien Philippe <bastien.philippe@soyhuce.fr>
2025-03-07 11:08:00 +0100

    Rename ViewExpectation to ViewExpect for brievety and easier reading

--- tests/ViewExpectationTest.php
+++ tests/ViewExpectationTest.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
 use Soyhuce\LaravelEmbuscade\Tests\fixtures\WelcomeLaravelComponent;
 use Soyhuce\LaravelEmbuscade\Tests\fixtures\WelcomeLivewireComponent;
-use Soyhuce\LaravelEmbuscade\ViewExpectation;
+use Soyhuce\LaravelEmbuscade\ViewExpect;
 
 beforeEach(function (): void {
     $this->expectedHtml = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR . 'welcome.html');
@@ -19,7 +19,7 @@ it('is created from TestResponse', function (): void {
         ->assertOk()
         ->expectView();
 
-    expect($expected)->toBeInstanceOf(ViewExpectation::class);
+    expect($expected)->toBeInstanceOf(ViewExpect::class);
     expect($expected->html)->toBe($this->expectedHtml);
 });
 
@@ -32,27 +32,27 @@ it('is created from view TestResponse', function (): void {
         ->assertOk()
         ->expectView();
 
-    expect($expected)->toBeInstanceOf(ViewExpectation::class);
+    expect($expected)->toBeInstanceOf(ViewExpect::class);
     expect($expected->html)->toBe($this->expectedHtml);
 });
 
 it('is created from TestView', function (): void {
     $expected = $this->view('test::welcome')->expectView();
 
-    expect($expected)->toBeInstanceOf(ViewExpectation::class);
+    expect($expected)->toBeInstanceOf(ViewExpect::class);
     expect($expected->html)->toBe($this->expectedHtml);
 });
 
 it('is created from TestComponent', function (): void {
     $expected = $this->component(WelcomeLaravelComponent::class)->expectView();
 
-    expect($expected)->toBeInstanceOf(ViewExpectation::class);
+    expect($expected)->toBeInstanceOf(ViewExpect::class);
     expect($expected->html)->toBe($this->expectedHtml);
 });
 
 it('is created from livewire test component', function (): void {
     $expected = Livewire::test(WelcomeLivewireComponent::class)->expectView();
 
-    expect($expected)->toBeInstanceOf(ViewExpectation::class);
+    expect($expected)->toBeInstanceOf(ViewExpect::class);
     $expected->first('head title')->toHaveText('Laravel');
 });
