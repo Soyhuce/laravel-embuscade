98bba1c4a1ddbdf459051c2feba1c87201142810
Bastien Philippe <bastien.philippe@soyhuce.fr>
2025-03-07 10:38:57 +0100

    Fix tests

--- tests/SelectorTest.php
+++ tests/SelectorTest.php
@@ -3,6 +3,8 @@
 use Soyhuce\LaravelEmbuscade\Embuscade;
 
 it('selects from selector', function (): void {
+    $this->artisan('view:clear');
+
     $this->view('test::selector')
         ->expectView()
         ->sole('@my-button')
@@ -10,10 +12,17 @@ it('selects from selector', function (): void {
 });
 
 it('customize the attribute from selector', function (): void {
+    $this->artisan('view:clear');
+
     Embuscade::selectorHtmlAttribute('data-test');
 
     $this->view('test::selector')
         ->expectView()
         ->sole('[data-test="my-button"]')
+        ->toContain('Click me');
+
+    $this->view('test::selector')
+        ->expectView()
+        ->sole('@my-button')
         ->toContain('Click me');
 });
